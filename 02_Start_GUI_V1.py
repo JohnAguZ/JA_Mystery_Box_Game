3587e5eaf5d771a1613ed95ae8214a729a4a6042
JohnAguZ <Aguirrezabalaj69731@masseyhigh.school.nz>
2020-03-30 14:50:25 +1300

    Error!!
    This is goving a simple error that I can't Fix. Plz help

--- 02_Start_GUI_V1.py
+++ 02_Start_GUI_V1.py
@@ -36,22 +36,22 @@ class Start:
         # Buttons go here...
         button_font = "Arial 12 bold"
         # Orange Low Stakes Button
-        self.lowstakes_button = Button(self.stakes_frame, text="Low ($5)",
-                                       command=lambda: self.to_game(1),
-                                       font=button_font, bg="#FC9003")
-        self.lowstakes_button.grid(row=0, column=0, pady=10)
+        self.low_stakes_button = Button(self.stakes_frame, text="Low ($5)",
+                                        command=lambda: self.to_game(1),
+                                        font=button_font, bg="#FC9003")
+        self.low_stakes_button.grid(row=0, column=0, pady=10)
 
         # Yellow Medium Stakes Button
-        self.lowstakes_button = Button(self.stakes_frame, text="Medium ($10)",
-                                       command=lambda: self.to_game(2),
-                                       font=button_font, bg="#FFFF00")
-        self.lowstakes_button.grid(row=0, column=1, padx=5, pady=10)
+        self.medium_stakes_button = Button(self.stakes_frame, text="Medium ($10)",
+                                           command=lambda: self.to_game(2),
+                                           font=button_font, bg="#FFFF00")
+        self.medium_stakes_button.grid(row=0, column=1, padx=5, pady=10)
 
         # Green High Stakes Button
-        self.lowstakes_button = Button(self.stakes_frame, text="High ($15)",
-                                       command=lambda: self.to_game(3),
-                                       font=button_font, bg="#00994D")
-        self.lowstakes_button.grid(row=0, column=2, padx=5, pady=10)
+        self.high_stakes_button = Button(self.stakes_frame, text="High ($15)",
+                                         command=lambda: self.to_game(3),
+                                         font=button_font, bg="#00994D")
+        self.high_stakes_button.grid(row=0, column=2, padx=5, pady=10)
 
         # Help Button
         self.help_button = Button(self.start_frame, text="Help / Rules",
@@ -60,11 +60,47 @@ class Start:
 
         #
 
-
     def to_game(self, stakes):
         starting_balance = self.start_amount_entry.get()
-        Game(self, stakes, starting_balance)
 
+        # Set error background colours (and assume that there are no
+        # errors at the start...
+        error_back = "#ffafaf"
+        has_errors = "no"
+
+        # Change background to white (For testing purposes)...
+        self.start_amount_entry.config(bg="white")
+        self.amount_error_label.config(text="")
+
+        try:
+            starting_balance = int(starting_balance)
+
+            if starting_balance < 5:
+                has_errors = "yes"
+                error_feedback = "Sorry, the least you" \
+                                 "can play with is $5"
+            elif starting_balance > 50:
+                has_errors = "yes"
+                error_feedback = "Too high! The most you can risk in " \
+                                 "this game is $50"
+            elif starting_balance < 10 and (stakes == 2 or stakes == 3):
+                has_errors = "yes"
+                error_feedback = "Sorry, you can oly afford to " \
+                                 "play a low stakes game."
+            elif starting_balance < 15 and stakes == 3:
+                has_errors = "yes"
+                error_feedback = "Sorry, you can oly afford to " \
+                                 "play a medium stakes game."
+        except ValueError:
+            has_errors = "yes":
+            error_feedback = "Pleas enter a dollar amount (No text / Decimals)"
+
+        if has_errors == "yes":
+            self.start_amount_entry.config(bg=error_back)
+            self.amount_error_label.config(text=error_feedback)
+
+        else:
+            Game(self, stakes, starting_balance)
 
 class Game:
     def __init__(self, partner, stakes, starting_balance):
