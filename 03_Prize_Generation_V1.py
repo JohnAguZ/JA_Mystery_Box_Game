9542ba5a69fdda2e57859ccdaed3234f529c4ae9
JohnAguZ <Aguirrezabalaj69731@masseyhigh.school.nz>
2020-04-02 14:43:49 +1300

    Update 03_Prize_Generation_V1.py
    Error Fix

--- 03_Prize_Generation_V1.py
+++ 03_Prize_Generation_V1.py
@@ -8,5 +8,5 @@ NUM_TRIALS = 10
 for item in range(0, NUM_TRIALS):
 
     # randint finds numbers between given endpoints, including both endpoints
-    prize_num = random.randint(1,4 )
+    prize_num = random.randint(1, 4)
     print(prize_num)
